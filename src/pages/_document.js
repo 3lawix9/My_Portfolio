24427bd0dd2f1799ebcf939d51e67f007c10e1a9
3lawix9 <92171974+3lawix9@users.noreply.github.com>
2023-02-27 16:36:10 +0300

    Update _document.js

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,13 +1,16 @@
 import { Html, Head, Main, NextScript } from 'next/document'
-import Link from 'next/link'
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <Link rel="stylesheet" 
-        href="https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/2.2.19/tailwind.min.css" integrity="sha512-DbcMdZvcJjLftzv62yP6WpU9cR1ZWwuZJP45J5U6W5PIM3dD6QnzaVh8fJ2Cll9dRXDn0AtZn7z1HSoG+Vohg==" crossOrigin="anonymous" />
-        </Head> 
+          <link
+            rel="stylesheet"
+            href="https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/2.2.19/tailwind.min.css"
+            integrity="sha512-DbcMdZvcJjLftzv62yP6WpU9cR1ZWwuZJP45J5U6W5PIM3dD6QnzaVh8fJ2Cll9dRXDn0AtZn7z1HSoG+Vohg=="
+            crossOrigin="anonymous"
+          />
+        </Head>
       
       <body class="debug-screens">
         <Main />
