1838d6b963941cc5348761840edd90602bcf10f7
3lawix9 <92171974+3lawix9@users.noreply.github.com>
2023-02-28 12:22:02 +0300

    Projects file changes

--- Components/Projects.jsx
+++ Components/Projects.jsx
@@ -3,37 +3,43 @@ import Image from 'next/image';
 
 
 const SKILLS_DATA = [
-  { image:'/coding.png', color: '#D84924', title: 'تم بناء هذا التطبيق للتعمق في أساسيات React js, وتم تطبيق جميع ما تتميز فيه React سواء من التعامل مع ال State و ال components و الإستخدام الأمثل لuseEffect وتسخيرها للتعامل مع اي إتصال يحصل مع ال API, وطريقة ربط React مع node js. ' },
-  { image: '/coding.png', color: '#0166B1', title: 'I did this project with React Tailwind' },
-  { image: '/coding.png', color: '#F14E32', title: 'I did this project with React Tailwind' },
+  { image:'/website-1.png', header: 'Salah times', title: 'I did this project with React Tailwind ', 
+  link:'https://github.com/3lawix9/My-first-website' , project: 'https://salawat.vercel.app'},
+  { image: '/website-2.png', header: 'Adventure website', title: 'I did this project with React Tailwind' , 
+  link:'https://github.com/3lawix9/Adventure-website', project: ''},
+  { image: '/website-3.png', header: 'Tech Bank website', title: 'I did this project with React Tailwind' , 
+  link:'https://github.com/3lawix9/AI-website', project: 'https://ai-website-sage.vercel.app'},
+  { image: '/website-4.png', header: 'Calculator', title: 'I did this project with React Tailwind' , 
+  link:'https://github.com/3lawix9/Calculator', project: 'https://calculator-eta-black.vercel.app'},
 ];
 
 function Projects() {
   return (
-    <div id="projects" className="w-full mb-28 p-2">
+    <div id="projects" className="w-full mb-28 p-2 ">
       <div className="max-w-[500px] lg:max-w-[900px] mx-auto flex flex-col justify-center h-full">
-        <p className="text-xl mb-8 tracking-widest text-center uppercase text-black">
+        <p className="text-xl mb-12 tracking-widest text-center uppercase text-black">
           Projects
         </p>
-        <div className="grid grid-cols-1 gap-8">
-          {SKILLS_DATA.map(({ image, title }, index) => (
+        <div className="grid xl:grid-cols-2 gap-10  ">
+          {SKILLS_DATA.map(({ image, title, header , link , project }, index) => (
 
-            <div key={index} className=" bg-blue-400  shadow-sm  rounded-xl">
-              <div className="relative h-52">
-                {/* <Image src={image} alt="Logo" layout='fill' objectFit='cover' /> */}
+            <div key={index} className=" shadow-md bg-gray-200  ">
+              <div className="relative h-52  ">
+                <Image className='rounded-t-lg' src={image} alt="Logo" fill style={{objectFit:"cover"}} />
+                
               </div>
-              <div className="p-16  mt-0 bg-gray-200 ">
-                <h1 className='mb-5 text-3xl items-center flex justify-center'>موقع تواصل إجتماعي</h1>
+              <div className="p-12  ">
+                <h1 className='mb-5 text-3xl items-center flex justify-center uppercase'>{header}</h1>
                 <h3>{title}</h3>
                 <div className="mt-6 flex justify-center items-center ">
                   <button className="px-4 py-2 mr-4 text-white bg-blue-500 rounded-md hover:bg-blue-600">
-                    <a >
+                    <a href={link} target="_blank">
                       Show Code
                     </a>
                   </button>
                   <button className="px-4 py-2 text-blue-500 bg-white border border-blue-500 rounded-md hover:text-white hover:bg-blue-500">
-                    <a >
-                      Show Project
+                    <a href={project} target="_blank" >
+                      Watch live
                     </a>
                   </button>
                 </div>
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,16 +3,8 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-          <link
-            rel="stylesheet"
-            href="https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/2.2.19/tailwind.min.css"
-            integrity="sha512-DbcMdZvcJjLftzv62yP6WpU9cR1ZWwuZJP45J5U6W5PIM3dD6QnzaVh8fJ2Cll9dRXDn0AtZn7z1HSoG+Vohg=="
-            crossOrigin="anonymous"
-          />
-        </Head>
-      
-      <body class="debug-screens">
+      <Head/>
+        <body class="debug-screens">
         <Main />
         <NextScript />
       </body>
