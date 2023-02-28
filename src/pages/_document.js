03ee20f2372b6e288229ac45768fdf7bc8814dbb
3lawix9 <92171974+3lawix9@users.noreply.github.com>
2023-02-28 14:52:35 +0300

    Update _document.js

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,12 +1,10 @@
 import { Html, Head, Main, NextScript } from 'next/document'
-import {style} from '../styles/globals.css'
 
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head/>
-      <link rel="stylesheet" href={style} />
         <body class="debug-screens">
         <Main />
         <NextScript />
