768673d55bc0eee7bcd0e0584449ec133084405d
3lawix9 <92171974+3lawix9@users.noreply.github.com>
2023-02-28 14:34:16 +0300

    Update _document.js

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,6 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head/>
+      <link rel="stylesheet" href="../styles/globals.css"></link>
         <body class="debug-screens">
         <Main />
         <NextScript />
