5bbc1d2dc2aff028aa7c2e56fbd6b2bb767c9e60
3lawix9 <92171974+3lawix9@users.noreply.github.com>
2023-02-28 17:07:16 +0300

    2

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,14 @@
 import '../styles/globals.css'
+import { css } from '@emotion/react';
+
+const globalStyles = css`
+@tailwind base;
+@tailwind components;
+@tailwind utilities;
+`;
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} css={globalStyles} />
 }
 
 export default MyApp
