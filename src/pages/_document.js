82e7ca3b5c121c08352cee0b0bc2ddc210ff2161
3lawix9 <92171974+3lawix9@users.noreply.github.com>
2023-02-28 17:03:43 +0300

    1

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,7 +4,10 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head/>
+      <Head>
+      <link rel="stylesheet" href="./styles/globals.css" />
+      </Head>
+
         <body class="debug-screens">
         <Main />
         <NextScript />
